00ed3b184203271d1eb76046c76d763f8e16fb47
Alexander Betke <main@alexinabox.de>
2024-12-27 01:19:15 +0100

    brainrot

--- InvisibleTutorial/Config.cs
+++ InvisibleTutorial/Config.cs
@@ -21,11 +21,11 @@ namespace InvisibleTutorial
 
         [Description("Hint displayed when a player goes invisible.")]
         //public Hint InvisibilityEnabled { get; private set; } = new("<b>You <color=green>are</color> now invisible.</b>", 3);
-        public string InvisibilityEnabled { get; private set; } = "<b>You <color=green>are</color> now invisible.</b>"
+        public string InvisibilityEnabled { get; private set; } = "<b>You <color=green>are</color> now invisible.</b>";
 
         [Description("Hint displayed when a player is visible again.")]
         //public Hint InvisibilityDisabled { get; private set; } = new("<b>You <color=red>are not</color> invisible anymore.</b>", 3);
-        public string InvisibilityDisabled { get; private set; } = "<b>You <color=red>are not</color> invisible anymore.</b>"
+        public string InvisibilityDisabled { get; private set; } = "<b>You <color=red>are not</color> invisible anymore.</b>";
 
         [Description("The unique id of the setting.")]
         public int KeybindId { get; set; } = 201;
